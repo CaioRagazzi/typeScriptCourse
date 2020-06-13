03f2e0e2a7f9024738686132d17e402f4e60da99
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2020-06-13 16:35:42 -0300

    Section13

---
+++ Section13/src/app.ts
@@ -0,0 +1,11 @@
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+
+}
+
+form.addEventListener('submit', searchAddressHandler);
